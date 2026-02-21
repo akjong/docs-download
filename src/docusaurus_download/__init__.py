ae10b316e4c282410a56ca70507bf61f4ffd5df7
Bob Liu <akagi201@gmail.com>
2026-02-21 15:12:02 +0800

    feat(docusaurus): add Docusaurus documentation scraper CLI and core module

---
+++ src/docusaurus_download/__init__.py
@@ -0,0 +1,5 @@
+"""Docusaurus documentation scraper."""
+
+from docusaurus_download.scraper import DocusaurusScraper, ScraperConfig, ScraperStats
+
+__all__ = ["DocusaurusScraper", "ScraperConfig", "ScraperStats"]
